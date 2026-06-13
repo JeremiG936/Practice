6cbd89d9d80e655772694ea4799e5ef54c8cd66c
JeremiG936 <jeremigonzalez1@gmail.com>
2026-06-13 16:10:11 +0000

    Placed calculator layout

---
+++ Basic_Calculator_P3/script3.js
@@ -0,0 +1,5 @@
+const btn1 = document.getElementById("btn-1");
+const displayer = document.getElementById("displayer");
+displayer.innerText = "Hello";
+
+console.log("Hello");
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Practice and Beginner Projects
+Project 1 - Static Website 
+Project 2 - Basic To-do List
+Project 3 - Basic Calculator
